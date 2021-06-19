3e5c34ec4897932ae3c5285c721aa0939da09558
masterjourney <masterjourney1@gmail.comy>
2021-06-19 16:22:23 +0300

    Final commit

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,8 @@ $(document).ready(function(){
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                dots: true,
+                arrows: false,
               }
             },
             {
@@ -30,6 +32,8 @@ $(document).ready(function(){
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                dots: true,
+                arrows: false,
               }
             }
             // You can unslick at a given breakpoint now by adding:
@@ -37,4 +41,87 @@ $(document).ready(function(){
             // instead of a settings object
           ]
     });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__details').eq(i).toggleClass('catalog-item__details_active');
+          });
+      });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+    //Modal windows
+
+    $('[data-modal="consultation"]').on('click', function() {
+      $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, .modal').fadeOut();
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+      });
+  });
+
+    
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+  
+  //Scroll and pageup
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+  
+  new WOW().init();
 });
